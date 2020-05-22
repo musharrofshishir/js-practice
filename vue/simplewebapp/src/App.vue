a5b1798b538eac943c2a7e7703fe932bf0fe2350
Musharrof Shishir <musharrofshishir@users.noreply.github.com>
2020-05-22 11:25:34 +0600

    vue application practice
    simple web-pack application with vue.js

---
+++ vue/simplewebapp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h2>This is my very first <span> Vue.js </span> application. I will update this file along with other practices.</h2>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  h2{
+    font-family: 'Avelin', Helvetica, sans-serif;
+    span{
+      color: #42b983;
+    }
+  }
+</style>
